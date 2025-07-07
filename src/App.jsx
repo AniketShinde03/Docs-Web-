4d5d61fa7918703fa4154a9bce1ae83bda1a4d46
Aniket Shinde <shindesam666@gmail.com>
2025-07-07 20:21:57 +0530

    Initial project upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='w-screen h-screen bg-zinc-800'>
+           
+      </div>
+    </>
+  )
+}
+
+export default App
